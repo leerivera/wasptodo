d7b14ca206debcf47c120a4e44d81bbc2d7379b9
leerivera <leerivera80@gmail.com>
2022-11-29 18:57:33 -0500

    get

--- src/client/MainPage.jsx
+++ src/client/MainPage.jsx
@@ -1,8 +1,36 @@
 
 
+import getTasks from '@wasp/queries/getTasks'
+import { useQuery } from '@wasp/queries'
+
 const MainPage = () => {
-  
-    return <div> Hello world </div>
-  
+  const { data: tasks, isFetching, error } = useQuery(getTasks)
+
+  return (
+    <div>
+      {tasks && <TasksList tasks={tasks} />}
+
+      {isFetching && 'Fetching...'}
+      {error && 'Error: ' + error}
+    </div>
+  )
 }
+
+const Task = (props) => {
+  return (
+    <div>
+      <input
+        type='checkbox' id={props.task.id}
+        checked={props.task.isDone}
+      />
+      {props.task.description}
+    </div>
+  )
+}
+
+const TasksList = (props) => {
+  if (!props.tasks?.length) return 'No tasks'
+  return props.tasks.map((task, idx) => <Task task={task} key={idx} />)
+}
+
 export default MainPage
